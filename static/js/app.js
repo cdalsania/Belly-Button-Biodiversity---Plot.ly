c9f2a6f5338e5637c985552fe2b1076d68a8ecbd
Crystal Dalsania <dalsania.crystal@gmail.com>
2021-02-02 20:38:25 -0500

    retrieviewing the data into respective variables

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,8 @@ var names = []
 var metadata = []
 var samples = []
 d3.json('static/data/samples.json').then(function(data) {
+
+            //retrieviewing the data into respective variables
+            names = data.names;
+            metadata = data.metadata;
+            samples = data.samples;
